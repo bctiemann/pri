5f7777397f840145f003325dbdb45ef1490d6dfa
Brian Tiemann <btman@mac.com>
2020-09-21 12:08:05 -0400

    Add view/context example

--- rentals/views.py
+++ rentals/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    # Example of passing context to the template in a class-based view
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['foo'] = 'bar'
+        return context
