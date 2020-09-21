33857a6fc1afca8a21928e9c7e46b2dc0047c1f2
Brian Tiemann <btman@mac.com>
2020-09-21 10:49:20 -0400

    Add examples of model properties/methods

--- rentals/models.py
+++ rentals/models.py
@@ -11,5 +11,15 @@ class Vehicle(models.Model):
     model = models.CharField(max_length=255, blank=True)
     vehicle_type = models.CharField(choices=VehicleType.choices, max_length=20, blank=True)
 
-    def __str__(self):
+    # Example of a model property which produces a derived value
+    @property
+    def vehicle_name(self):
         return f'{self.make} {self.model}'
+
+    # Example of a model method taking one or more params - invoke like vehicle.is_available(date_start, date_end)
+    def is_available(self, date_start, date_end):
+        # logic to check availability goes here
+        return True
+
+    def __str__(self):
+        return self.vehicle_name
