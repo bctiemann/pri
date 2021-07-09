805714b03c687b85fdc2d8407d59918f0af80191
Brian Tiemann <btman@mac.com>
2021-07-09 17:16:44 -0400

    Working front page

--- marketing/views.py
+++ marketing/views.py
@@ -1,11 +1,15 @@
 from django.views.generic import TemplateView
 
+from fleet.models import Vehicle, VehicleType, VehicleStatus
+
 
 class HomeView(TemplateView):
     template_name = 'front_site/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['foo'] = 'bar'
-        # context['cars'] = Vehicle.objects.filter(vehicle_type=VehicleType.CAR.value)
+        ready_vehicles = Vehicle.objects.filter(status=VehicleStatus.READY.value).order_by('-weighting')
+        context['cars'] = ready_vehicles.filter(vehicle_type=VehicleType.CAR.value)
+        context['bikes'] = ready_vehicles.filter(vehicle_type=VehicleType.BIKE.value)
+        context['showcase_vehicle'] = ready_vehicles.order_by('?').first()
         return context
