9791e4e73ba3c77c69b00d4d06f98d91a0d9ea7c
Brian Tiemann <btman@mac.com>
2021-07-28 07:45:30 -0400

    Filter calendar events efficiently to avoid db queries

--- consignment/utils.py
+++ consignment/utils.py
@@ -1,6 +1,7 @@
 import calendar
 import datetime
 import pytz
+from dateutil.relativedelta import relativedelta
 
 from django.conf import settings
 
@@ -26,17 +27,27 @@ class EventCalendar(calendar.HTMLCalendar):
             return '<td class="%s">&nbsp;</td>' % self.cssclass_noday
         focus_date = datetime.datetime(self.year, self.month, day)
         focus_date = pytz.timezone(settings.TIME_ZONE).localize(focus_date)
-        rentals = Rental.objects.filter(out_at__lte=focus_date, back_at__gte=focus_date, vehicle__external_owner=self.consigner)
-        consigner_reservations = ConsignmentReservation.objects.filter(out_at__lte=focus_date, back_at__gte=focus_date)
-        if self.vehicle:
-            rentals = rentals.filter(vehicle=self.vehicle)
-            consigner_reservations = consigner_reservations.filter(vehicle=self.vehicle)
+
+        rentals = set(filter(lambda r: r.out_at.astimezone(pytz.timezone(settings.TIME_ZONE)).date() <= focus_date.date() <= r.back_at.astimezone(pytz.timezone(settings.TIME_ZONE)).date(), self.rentals))
+        consigner_reservations = set(filter(lambda r: r.out_at.astimezone(pytz.timezone(settings.TIME_ZONE)).date() <= focus_date.date() <= r.back_at.astimezone(pytz.timezone(settings.TIME_ZONE)).date(), self.consigner_reservations))
+
         classes = self.cssclasses[weekday]
-        if rentals.exists():
+        if rentals:
             classes += ' rental'
-        if consigner_reservations.exists():
+        if consigner_reservations:
             classes += ' reservation'
         return '<td class="%s">%d</td>' % (classes, day)
 
     def formatmonth(self, withyear=True):
+        month_start = datetime.datetime(self.year, self.month, 1)
+        month_start = pytz.timezone(settings.TIME_ZONE).localize(month_start)
+        next_month_start = month_start + relativedelta(months=1)
+
+        self.rentals = Rental.objects.filter(out_at__lte=next_month_start, back_at__gte=month_start)
+        self.consigner_reservations = ConsignmentReservation.objects.filter(out_at__lte=next_month_start, back_at__gte=month_start)
+        self.rentals = self.rentals.filter(vehicle__external_owner=self.consigner)
+        if self.vehicle:
+            self.rentals = rentals.filter(vehicle=self.vehicle)
+            self.consigner_reservations = consigner_reservations.filter(vehicle=self.vehicle)
+
         return super().formatmonth(self.year, self.month, withyear=withyear)
