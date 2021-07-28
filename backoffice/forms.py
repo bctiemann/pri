d8007bb6c676187d83be6f28ac87d69baca0cf10
Brian Tiemann <btman@mac.com>
2021-07-27 21:34:53 -0400

    Exclude slug from VehicleForm

--- backoffice/forms.py
+++ backoffice/forms.py
@@ -4,12 +4,14 @@ from fleet.models import Vehicle, VehicleMarketing
 from consignment.models import Consigner
 
 
+# TODO: Add slug to the visible form fields and set on both models
+
 class VehicleForm(forms.ModelForm):
     external_owner = forms.ModelChoiceField(queryset=Consigner.objects.all(), empty_label='PRI', required=False)
 
     class Meta:
         model = Vehicle
-        fields = '__all__'
+        exclude = ('slug',)
 
 
 class VehicleMarketingForm(forms.ModelForm):
