5caabc3aa71ada9a2e7f93a8f75a9fdc6a2824ea
Brian Tiemann <btman@mac.com>
2021-08-04 10:25:38 -0400

    Improve VehicleMarketing admin

--- fleet/admin.py
+++ fleet/admin.py
@@ -22,6 +22,33 @@ class VehicleMarketingAdmin(admin.ModelAdmin):
     }
     inlines = (VehiclePictureInline,)
     prepopulated_fields = {"slug": ("model",)}
+    fieldsets = (
+        (None, {
+            'fields': ('vehicle_id',)
+        }),
+        ('Redundant with Vehicle - update corresponding record manually', {
+            'fields': ('make', 'model', 'year', 'slug', 'vehicle_type', 'status', 'weighting',)
+        }),
+        ('Images', {
+            'fields': (
+                'showcase_image', 'showcase_width', 'showcase_height',
+                'thumbnail_image', 'thumbnail_width', 'thumbnail_height',
+                'mobile_thumbnail_image', 'mobile_thumbnail_width', 'mobile_thumbnail_height',
+                'inspection_image', 'inspection_width', 'inspection_height',
+            )
+        }),
+        ('Statistics', {
+            'fields': (
+                'horsepower', 'torque', 'top_speed', 'transmission_type', 'gears', 'location', 'tight_fit',
+                'blurb', 'specs', 'origin_country',
+            )
+        }),
+        ('Price fields', {
+            'fields': (
+                'price_per_day', 'discount_2_day', 'discount_3_day', 'discount_7_day', 'security_deposit', 'miles_included',
+            )
+        })
+    )
 
 
 admin.site.register(Vehicle, VehicleAdmin)
