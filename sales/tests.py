910fc9cdb4e4c435569613c6d5ccbf1748742f18
Brian Tiemann <btman@mac.com>
2021-12-11 12:05:41 -0500

    Add test for price with customer discount

--- sales/tests.py
+++ sales/tests.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from django.test import TestCase
 
 from fleet.models import VehicleMarketing
-from users.models import Customer
+from users.models import Customer, User
 from sales.models import TaxRate, Coupon
 from sales.utils import RentalPriceCalculator
 
@@ -24,7 +24,11 @@ class RentalPriceCalculatorTestCase(TestCase):
             postal_code='07430',
             total_rate=0.06625,
         )
+        self.user_1 = User.objects.create_user(
+            email='email@test.com',
+        )
         self.customer_1 = Customer.objects.create(
+            user=self.user_1,
             discount_pct=10,
         )
         self.coupon_1 = Coupon.objects.create(
@@ -33,6 +37,9 @@ class RentalPriceCalculatorTestCase(TestCase):
         )
 
     def test_get_rental_price_data(self):
+        """
+        2-day rental, 200 extra miles, no coupon, no customer discount
+        """
         vehicle_marketing = self.vehicle_1
         num_days = 2
         coupon_code = None
@@ -44,9 +51,12 @@ class RentalPriceCalculatorTestCase(TestCase):
         )
         price_data = rental_price_calculator.get_price_data()
 
-        self.assertEqual(price_data['total_with_tax'], Decimal('1215.53'))
+        self.assertEqual(price_data['total_with_tax'], Decimal('1311.49'))
 
     def test_get_rental_price_data_with_coupon(self):
+        """
+        2-day rental, 200 extra miles, $15 coupon, no customer discount
+        """
         vehicle_marketing = self.vehicle_1
         num_days = 2
         coupon_code = 'TEST'
@@ -58,5 +68,22 @@ class RentalPriceCalculatorTestCase(TestCase):
         )
         price_data = rental_price_calculator.get_price_data()
 
-        self.assertEqual(price_data['total_with_tax'], Decimal('1201.13'))
+        self.assertEqual(price_data['total_with_tax'], Decimal('1295.49'))
+
+    def test_get_rental_price_data_with_customer_discount(self):
+        """
+        2-day rental, 200 extra miles, no coupon, 10% customer discount
+        """
+        vehicle_marketing = self.vehicle_1
+        num_days = 2
+        coupon_code = None
+        email = 'email@test.com'
+        extra_miles = 200
+        tax_zip = '07430'
+        rental_price_calculator = RentalPriceCalculator(
+            vehicle_marketing, num_days, coupon_code, email, extra_miles, tax_zip,
+        )
+        price_data = rental_price_calculator.get_price_data()
+
+        self.assertEqual(price_data['total_with_tax'], Decimal('1215.53'))
 
