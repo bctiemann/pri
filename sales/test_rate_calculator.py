d8d668f062c0a8b7cba153da93b40dc003d09dfc
Brian Tiemann <btman@mac.com>
2021-12-17 11:36:18 -0500

    Parameterized tests with pytest

--- sales/test_rate_calculator.py
+++ sales/test_rate_calculator.py
@@ -1,4 +1,5 @@
 import pytest
+# from mock_django.query import QuerySetMock
 
 from django.test import TestCase
 
@@ -20,10 +21,10 @@ from sales.utils import RentalPriceCalculator
 @pytest.fixture
 def vehicle_mock_1(mocker):
     vehicle_1 = mocker.MagicMock(spec=VehicleMarketing)
-    vehicle_1.return_value.price_per_day = 950
-    vehicle_1.return_value.discount_2_day = 10
-    vehicle_1.return_value.discount_3_day = 20
-    vehicle_1.return_value.discount_7_day = 40
+    vehicle_1.price_per_day = 950
+    vehicle_1.discount_2_day = 10
+    vehicle_1.discount_3_day = 20
+    vehicle_1.discount_7_day = 40
     # vehicle_1.return_value = mocker.MagicMock()
     return vehicle_1
 
@@ -42,8 +43,18 @@ def coupon_mock_1(mocker):
     coupon_1 = mocker.MagicMock(spec=Coupon)
     coupon_1.amount = 15.00
     # coupon_1.get_discount_value = mocker.MagicMock()
-    coupon_1.return_value.get_discount_value.return_value = 15.00
+    coupon_1.get_discount_value.return_value = 15.00
     return coupon_1
+
+
+# @pytest.fixture
+# def coupon_queryset_mock_1(mocker, coupon_mock_1):
+#     coupon_queryset_1 = QuerySetMock(Coupon, coupon_mock_1)
+#     # coupon_queryset_1 = mocker.MagicMock(spec=Coupon.objects)
+#     # coupon_queryset_1.first = mocker.MagicMock()
+#     # coupon_queryset_1.first.return_value = coupon_mock_1
+#     print('fixture', coupon_queryset_1.first.return_value.get_discount_value)
+#     return coupon_queryset_1
 
 
 @pytest.fixture
@@ -53,61 +64,47 @@ def customer_mock_1(mocker):
     return customer_1
 
 
-@pytest.fixture(autouse=True)
-def _mock_db_connection(mocker, tax_rate_1, coupon_mock_1, customer_mock_1):
-    mocker.patch('sales.models.TaxRate.objects.get_or_create', tax_rate_1)
-    mocker.patch('sales.models.Coupon.objects.get', coupon_mock_1)
-    mocker.patch('users.models.Customer.objects.get', customer_mock_1)
+# @pytest.fixture(autouse=True)
+# def _mock_db_connection(mocker, tax_rate_1, coupon_queryset_mock_1, customer_mock_1):
+#     mocker.patch('sales.models.TaxRate.objects.get_or_create', tax_rate_1)
+#     mocker.patch('sales.models.Coupon.objects.filter', coupon_queryset_mock_1)
+#     mocker.patch('users.models.Customer.objects.filter', customer_mock_1)
+#     # mocker.patch()
 
 
-@pytest.fixture
-def num_days():
-    return 2
-
-
-@pytest.fixture
-def email():
-    return 'btman@mac.com'
-
-
-@pytest.fixture
-def extra_miles():
-    return 200
-
-
-@pytest.fixture
-def tax_zip():
-    return '07430'
-
-
-@pytest.mark.parametrize(
-    "vehicle, num_days, coupon_code, email, extra_miles, tax_zip",
-    [(vehicle_mock_1, 1, None, None, 200, "07430"), (vehicle_mock_1, 2, None, None, 200, "07430")]
-)
-def test_something(vehicle, num_days, coupon_code, email, extra_miles, tax_zip):
-    assert num_days > 0
+# @pytest.fixture
+# def num_days():
+#     return 2
+#
+#
+# @pytest.fixture
+# def email():
+#     return 'btman@mac.com'
+#
+#
+# @pytest.fixture
+# def extra_miles():
+#     return 200
+#
+#
+# @pytest.fixture
+# def tax_zip():
+#     return '07430'
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    'vehicle, num_days, coupon_code, email, extra_miles, tax_zip',
+    'vehicle, num_days, coupon_code, email, extra_miles, tax_zip, expected_total',
     [
-        (
-            vehicle_mock_1,
-            2,
-            None,
-            None,
-            200,
-            '07430',
-        ),
+        ('vehicle_mock_1', 2, None, None, 200, '07430', '2175.15'),
     ],
     # indirect=True
 )
-def test_get_rental_price_data(vehicle, num_days, coupon_code, email, extra_miles, tax_zip):
+def test_get_rental_price_data(request, vehicle, num_days, coupon_code, email, extra_miles, tax_zip, expected_total):
     num_days = 2
     # vehicle_marketing, num_days, extra_miles, coupon_code = coupon_code, email = email, tax_zip = tax_zip,
     rental_price_calculator = RentalPriceCalculator(
-        vehicle,
+        request.getfixturevalue(vehicle),
         num_days,
         extra_miles,
         coupon_code=coupon_code,
@@ -117,8 +114,7 @@ def test_get_rental_price_data(vehicle, num_days, coupon_code, email, extra_mile
     print(rental_price_calculator)
     price_data = rental_price_calculator.get_price_data()
     print(price_data)
-    assert None == None
-
+    assert str(price_data['total_with_tax']) == expected_total
 
 # # class RentalPriceCalculatorTestCase(TestCase):
 # # @pytest.mark.django_db
