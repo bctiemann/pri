ac528e83a003016cf9a72f76807959dea5fce1d0
Brian Tiemann <btman@mac.com>
2022-01-19 10:53:46 -0500

    Service model admins

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from service.models import ServiceItem, ScheduledService, IncidentalService
+
+
+class ServiceItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class ScheduledServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vehicle', 'service_item', 'done_at', 'done_mileage',)
+
+
+class IncidentalServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vehicle', 'done_at', 'mileage',)
+
+
+admin.site.register(ServiceItem, ServiceItemAdmin)
+admin.site.register(ScheduledService, ScheduledServiceAdmin)
+admin.site.register(IncidentalService, IncidentalServiceAdmin)
