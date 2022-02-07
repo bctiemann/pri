7186fa23577778f2b261ac841e89d045ee1dd984
Brian Tiemann <btman@mac.com>
2022-02-07 18:01:34 -0500

    Add CC fields to confirm form

--- customer_portal/forms.py
+++ customer_portal/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 
+from sales.enums import get_exp_year_choices, get_exp_month_choices
 from sales.models import BaseReservation
 from users.models import Customer
 
@@ -12,6 +14,19 @@ class PasswordForm(forms.Form):
 class ReservationCustomerInfoForm(forms.ModelForm):
 
     confirmation_code = forms.CharField(widget=forms.HiddenInput())
+
+    cc_number = forms.CharField(required=False)
+    cc_exp_yr = forms.ChoiceField(choices=get_exp_year_choices(since_founding=True, allow_null=False))
+    cc_exp_mo = forms.ChoiceField(choices=get_exp_month_choices(allow_null=False))
+    cc_cvv = forms.CharField(required=False)
+    cc_phone = PhoneNumberField(region='US', required=False)
+
+    cc2_number = forms.CharField(required=False)
+    cc2_exp_yr = forms.ChoiceField(choices=get_exp_year_choices(since_founding=True, allow_null=True), required=False)
+    cc2_exp_mo = forms.ChoiceField(choices=get_exp_month_choices(allow_null=True), required=False)
+    cc2_cvv = forms.CharField(required=False)
+    cc2_phone = PhoneNumberField(region='US', required=False)
+    cc2_instructions = forms.CharField(widget=forms.Textarea(), required=False)
 
     def __init__(self, *args, reservation=None, **kwargs):
         super().__init__(*args, **kwargs)
