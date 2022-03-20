ecbaa7cce2566ded4ac80442f70b3c45c5d6b574
Brian Tiemann <btman@mac.com>
2022-03-19 23:33:39 -0400

    Simplified response

--- users/views.py
+++ users/views.py
@@ -78,19 +78,8 @@ class PasswordResetView(PasswordResetView):
 
     def form_valid(self, form):
         logger.info(f'{form.cleaned_data["email"]} requested a password reset')
-        # return super().form_valid(form)
-        opts = {
-            "use_https": self.request.is_secure(),
-            "token_generator": self.token_generator,
-            "from_email": self.from_email,
-            "email_template_name": self.email_template_name,
-            "subject_template_name": self.subject_template_name,
-            "request": self.request,
-            "html_email_template_name": self.html_email_template_name,
-            "extra_email_context": self.extra_email_context,
-        }
-        form.save(**opts)
-        return JsonResponse({})
+        response = super().form_valid(form)
+        return JsonResponse({'success': True})
 
 
 class PasswordResetConfirmView(PasswordResetConfirmView):
