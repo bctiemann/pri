d2c56a45daed88e82b6b1b36ba2aeabc1510d65a
Brian Tiemann <btman@mac.com>
2022-12-31 19:40:40 -0500

    Template and form cleanup for Stripe charges

--- backoffice/forms.py
+++ backoffice/forms.py
@@ -709,7 +709,9 @@ class StripeChargeForm(CSSClassMixin, forms.ModelForm):
     phone_fields = ('phone',)
     short_fields = (
         'zip', 'home_phone', 'mobile_phone', 'work_phone', 'fax', 'insurance_company_phone', 'discount_pct',
-        'cc_cvv', 'cc_phone', 'cc2_cvv', 'cc2_phone',
+        'cc_cvv', 'cc2_cvv', 'phone',
+        # For "Charge" view (StripeChargeChargeView) only
+        'amount', 'cc_zip',
     )
 
     CHARGE_TYPE_CHOICES = (
@@ -717,7 +719,9 @@ class StripeChargeForm(CSSClassMixin, forms.ModelForm):
         (False, 'Auth only'),
     )
 
+    amount = forms.DecimalField(required=True)
     charge_type = forms.ChoiceField(choices=CHARGE_TYPE_CHOICES, initial=False)
+    cc_address = forms.CharField(widget=forms.Textarea(), required=False)
     cc_exp_yr = forms.ChoiceField(choices=get_exp_year_choices(since_founding=True, allow_null=False))
     cc_exp_mo = forms.ChoiceField(choices=get_exp_month_choices(allow_null=False))
     stripe_token = forms.CharField(widget=forms.HiddenInput(), required=False)
